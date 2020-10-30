77dd875ff04179a482cb91d8a30e409b40834847
Zdeněk Sotolář <sotolar.zdenek@gmail.com>
2020-10-30 15:06:19 +0100

    finis projektu

--- album/start/script.js
+++ album/start/script.js
@@ -2,10 +2,33 @@ let fetchUsersBtn = document.querySelector("#fetch-users")
 let contentDiv = document.querySelector("#content")
 let history = document.querySelector("#history")
 let filterForm = document.querySelector("#filter")
+let resetFilter = document.querySelector("#reset-filter")
 
 // result = alert()
 
 // let ahoj = "ahoj"
+
+function prepareAPIUrl() { // string -> URL adresu včetne GET parametru
+    let filter = JSON.parse(localStorage.getItem("filter"))
+
+    let apiURL = "https://randomuser.me/api/"
+
+    let parameters = []
+
+    if (filter.limit) {
+        parameters.push(`results=${filter.limit}`)
+    }
+
+    if (filter.include.length > 0) { // ["id", "name"] -> inc=id,name
+        parameters.push(`inc=${filter.include.join(",")}`)
+    }
+
+    if (parameters.length > 0) {
+        apiURL = `${apiURL}?${parameters.join("&")}`
+    }
+
+    return apiURL
+}
 
 function runApp() {
     // inicializovat localStorage o uživatele
@@ -14,11 +37,26 @@ function runApp() {
     }
 
     // inicializovat filter
-    if (localStorage.getItem("filter") === null) {
+    let filterStorage = localStorage.getItem("filter")
+    if (filterStorage === null) {
         localStorage.setItem("filter", JSON.stringify({
             limit: "",
             include: []
         }))
+    } else {
+        let data = JSON.parse(filterStorage) 
+
+        // input -> string document.querySelector
+        filterForm.querySelector("input[name='limit']").value = data.limit
+
+        // include -> array
+        filterForm.querySelectorAll("select[name='include'] option").forEach(option => {
+            //console.log(option.value)
+            //console.log(option.value, data.include.indexOf(option.value))
+            if (data.include.indexOf(option.value) !== -1) {
+                option.selected = true
+            }
+        })
     }
 
     // Historie vyhledavani
@@ -51,6 +89,15 @@ function runApp() {
 
 runApp()
 
+//console.log(prepareAPIUrl())
+
+resetFilter.addEventListener("click", event => {
+    localStorage.setItem("filter", JSON.stringify({
+        limit: "",
+        include: []
+    }))
+})
+
 filterForm.addEventListener("submit", event => {
     event.preventDefault()
 
@@ -63,7 +110,7 @@ filterForm.addEventListener("submit", event => {
 })
 
 fetchUsersBtn.addEventListener("click", function() {
-    fetch("https://randomuser.me/api/?results=4")
+    fetch(prepareAPIUrl())
         .then(response => response.json())
         .then(data => {
             let users = data.results
